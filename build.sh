99c3c37bb3aa3465a036238782d52c4d68a2bcf1
Mitch Murphy <mitch.murphy@gmail.com>
2021-04-27 11:48:50 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-git clone https://github.com/memsharded/four-c-example.git
-cd four-c-example
-mkdir build
-cd build
- 
 conan install .. -s compiler=gcc -s compiler.version=4.8 -s compiler.libcxx=libstdc++11 --build
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
