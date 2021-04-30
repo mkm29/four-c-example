197fffaee518b379185f1558c8e9a28030dd2f18
Mitch Murphy <mitch.murphy@gmail.com>
2021-04-30 12:19:02 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir build && cd build
 
-conan install .. -s compiler=clang -s compiler.version=10 -s compiler.libcxx=libstdc++11 --build
+conan install .. -s compiler=gcc -s compiler.version=8.3 -s compiler.libcxx=libstdc++11 --build
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
 cmake --build .
